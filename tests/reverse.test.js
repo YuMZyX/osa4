772be0d8be91f4865dd4adf27bf83ecd015753a5
YuMZyX <joni1koskinen@gmail.com>
2023-09-26 16:45:53 +0300

    Lisätty tehtävät 4.3 - 4.7

--- tests/reverse.test.js
+++ tests/reverse.test.js
@@ -1,18 +1,18 @@
 const reverse = require('../utils/for_testing').reverse
 
-test('reverse of a', () => {
+test.skip('reverse of a', () => {
   const result = reverse('a')
 
   expect(result).toBe('a')
 })
 
-test('reverse of react', () => {
+test.skip('reverse of react', () => {
   const result = reverse('react')
 
   expect(result).toBe('tcaer')
 })
 
-test('reverse of saippuakauppias', () => {
+test.skip('reverse of saippuakauppias', () => {
   const result = reverse('saippuakauppias')
 
   expect(result).toBe('saippuakauppias')
